39f21d40d6352a337e7f121c1a92a61e0908c0db
Fredrik Linde <gearfreak@hotmail.se>
2018-04-12 14:23:35 +0200

    Added SSIM CUDA implementation.
    Added functions to run SSIM on the GPU. Requires the SSIM dll, tbb.dll and OpenCV dll to be placed in the Plugin folder. A link to these will be provided in the near future.

---
+++ BachelorThesis/Assets/Scripts/Metrics/SSIM.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.IO;
+using UnityEngine;
+
+public class SSIM : MonoBehaviour
+{
+
+    #region Public Variables
+
+    #endregion
+
+    #region Private Variables
+
+    #endregion
+
+    #region Public Functions
+    // To be used for comparing stored images.
+    public static float compute_mssim_string(string imgpath1, string imgpath2)
+    {
+        // Adds the application data path which leads to the project's asset folder.
+        string path1 = Application.dataPath + "/" + imgpath1;
+        string path2 = Application.dataPath + "/" + imgpath2;
+
+        // Does any of the images exist?
+        if (File.Exists(path1) && File.Exists(path2))
+        {
+            return OpenCVInterop.mssim_string(path1, path2);
+        }
+        else
+        {
+            Debug.Log("Could not find the following image paths:\n" + path1 + "\n" + path2);
+            return -1.0f;
+        }
+    }
+
+    // To be used when comparing screenshots in run-time.
+    public static float compute_mssim_byte(byte[] imgarray1, byte[] imgarray2, int w, int h)
+    {
+        // Allocate unmanaged memory.
+        IntPtr ua1 = Marshal.AllocHGlobal(Marshal.SizeOf(imgarray1[0]) * imgarray1.Length);
+        IntPtr ua2 = Marshal.AllocHGlobal(Marshal.SizeOf(imgarray2[0]) * imgarray2.Length);
+
+        // Copy content from byte arrays into the unmanaged memory.
+        Marshal.Copy(imgarray1, 0, ua1, imgarray1.Length);
+        Marshal.Copy(imgarray2, 0, ua2, imgarray2.Length);
+
+        float result = OpenCVInterop.mssim_byte(ua1, ua2, w, h);
+
+        // De-allocate the unmanaged memory.
+        Marshal.FreeHGlobal(ua1);
+        Marshal.FreeHGlobal(ua2);
+
+        return result;
+    }
+    #endregion
+
+    #region Private Functions
+
+    #endregion
+
+    // Use this for initialization
+    void Start()
+    {
+        float index = compute_mssim_string("Images/BoatOriginal.jpg", "Images/BoatNoise.jpg");
+        Debug.Log("MSSIM: " + index);
+    }
+}
+
+// Define the functions which can be called from the .dll.
+internal static class OpenCVInterop
+{
+    [DllImport("SSIM")]
+    internal static extern float mssim_string(string img1, string img2);
+    [DllImport("SSIM")]
+    internal static extern float mssim_byte(IntPtr img1, IntPtr img2, int w, int h);
+}
---
+++ BachelorThesis/Assets/Scripts/Metrics/ScreenCapturer.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class ScreenCapturer : MonoBehaviour {
+
+    #region Public Variables
+    public int capture_width;
+    public int capture_height;
+    public GameObject obj;
+    // Optimize for many screenshots. No objects will be destroyed so future screenshots are fast.
+    public bool optimize;
+    #endregion
+
+    #region Private Variables
+    private Rect _rect;
+    private RenderTexture _render_texture;
+    private Texture2D _screenshot;
+
+    private Camera _main_camera;
+    #endregion
+
+    #region Public Functions
+    public byte[] capture_screenshot()
+    {
+        // Create screenshot objects if necessary.
+        if (_render_texture == null)
+        {
+            // creates off-screen render texture that can rendered into.
+            _rect = new Rect(0, 0, capture_width, capture_height);
+            _render_texture = new RenderTexture(capture_width, capture_height, 24);
+            _screenshot = new Texture2D(capture_width, capture_height, TextureFormat.RGB24, false);
+        }
+
+        // Set the camera render target texture.
+        _main_camera.targetTexture = _render_texture;
+        _main_camera.Render();
+
+        // read pixels will read from the currently active render texture so make our offscreen 
+        // render texture active and then read the pixels
+        RenderTexture.active = _render_texture;
+        _screenshot.ReadPixels(_rect, 0, 0);
+
+        // reset active camera texture and render texture
+        _main_camera.targetTexture = null;
+        RenderTexture.active = null;
+
+        byte[] fileData = null;
+        fileData = _screenshot.EncodeToJPG();
+
+        // cleanup if needed
+        if (optimize == false)
+        {
+            Destroy(_render_texture);
+            _render_texture = null;
+            _screenshot = null;
+        }
+
+        return fileData;
+    }
+    #endregion
+
+    #region Private Functions
+
+    #endregion
+    private void Start()
+    {
+        _main_camera = Camera.main;
+        byte[] image1 = capture_screenshot();
+        obj.SetActive(false);
+        byte[] image2 = capture_screenshot();
+        float index = SSIM.compute_mssim_byte(image1, image2, capture_width, capture_height);
+        Debug.Log("MSSIM: " + index);
+    }
+
+    void Update () {
+    }
+}
